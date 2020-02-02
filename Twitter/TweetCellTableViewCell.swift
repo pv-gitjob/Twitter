f73bed30142b0ac96bfcf5951d7aa259b6e69571
Praveen Vandeyar <praveenvandeyar@gmail.com>
2020-02-01 23:23:24 -0800

    Complete Part 1

---
+++ Twitter/TweetCellTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  TweetCellTableViewCell.swift
+//  Twitter
+//
+//  Created by Praveen V on 2/1/20.
+//  Copyright © 2020 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetCellTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var tweetContentLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+}
